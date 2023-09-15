d97bd9c1e931b941fc30c8fb4db532124c3f1fca
Julia <uliakrishtapovich@gmail.com>
2023-09-15 14:34:24 +0200

    finshed-activebox

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,28 @@
-console.log("aloha, bro");
+const mySwiper = new Swiper('.image-slider', {
+    pagination:{
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    slidesPerView:1,
+    slidesPerGroup:1,
+
+    breakpoints: {
+        1440:{
+            slidesPerView:1,
+        },
+        1024:{
+            slidesPerView:1,
+        },
+        320: {
+            slidesPerView:1,
+            slidesPerGroup:1,
+        },
+    }
+});
+$(document).ready(function() {
+    $('.header__burger').click(function(event) {
+        $('.header__burger,.header_nav').toggleClass('active');
+        $('body').toggleClass('lock');
+         });
+    });
+
